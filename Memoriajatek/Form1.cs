d1ea386ab8bc736c50dd78004473e90bea1532db
fulopd <fulopdaniel@gmail.com>
2020-04-16 20:22:54 +0200

    Véletlenszerű objektumok és az ikonlista elkészítése

--- Memoriajatek/Form1.cs
+++ Memoriajatek/Form1.cs
@@ -12,6 +12,19 @@ namespace Memoriajatek
 {
     public partial class Form1 : Form
     {
+
+        // A Random objektum fogja kiválasztani az ikonokat a cellákba
+        Random random = new Random();
+
+        // Mindegyik betű egy érdekes ikon
+        // a Webdings font készletében
+        // és mindegyik ikon kétszer szerepel a listában
+        List<string> icons = new List<string>()
+        {
+            "j", "j", "Y", "Y", ",", ",", "!", "!",
+            "-", "-", "#", "#", "$", "$", "b", "b"
+        };
+
         public Form1()
         {
             InitializeComponent();
