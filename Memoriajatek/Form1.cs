120a74069836cc9357f3c0261794258582c36e5b
fulopd <fulopdaniel@gmail.com>
2020-04-16 21:38:43 +0200

    6. lépés: Stopper hozzáadása

--- Memoriajatek/Form1.cs
+++ Memoriajatek/Form1.cs
@@ -25,14 +25,24 @@ namespace Memoriajatek
             "-", "-", "#", "#", "$", "$", "b", "b"
         };
 
+
+        // firstClicked az elsőnek kiválasztott címkére fog mutatni 
+        // amire a játékos klikkelt, de ennek értéke addig null 
+        // amíg a játékos rá nem kattintott egy címkére
+        Label firstClicked = null;
+
+        // secondClicked a második címkére fog hivatkozni 
+        // amire a játékos kattint
+        Label secondClicked = null;
+
         
-       
-       
+
+
         public Form1()
         {
             InitializeComponent();
             AssignIconsToSquares();
-
+            
         }
 
 
@@ -67,6 +77,14 @@ namespace Memoriajatek
         /// <param name="e"></param>
         private void label_Click(object sender, EventArgs e)
         {
+
+            
+            // The stopper csak akkor működik, ha két nem egyező 
+            // ikont jelenített meg a játékosnak,
+            // addig minden klikkelést figyelmen kívül hagy
+            if (timer1.Enabled == true)
+                return;
+
             Label clickedLabel = sender as Label;
 
             if (clickedLabel != null)
@@ -77,9 +95,65 @@ namespace Memoriajatek
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
 
-                clickedLabel.ForeColor = Color.Black;
+                // Ha a firstClicked értéke null, akkor ez az első ikon 
+                // a párok közül, amit a játékos választott ki,
+                // így a firstClicked a kiválasztott címkével lesz egyenlő 
+                // a szöveg színe fekete lesz, majd visszatér
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+
+                    return;
+                }
+
+                // Ha a stopper még nem indult el
+                // és a firstClicked értéke nem null,
+                // így a második ikonnak, amire a játékos klikkelt
+                // a színe fekete lesz
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                // Ha a játékos két különböző ikonra kattint,
+                // akkor indítsa el a Stopper időmérését 
+                // (várni fog háromnegyed másodpercet 
+                // azután elrejti az ikonokat)
+                timer1.Start();
+            
+
             }
+
         }
+        /// <summary>
+        /// Az stopper elindul, ha a játékos
+        /// két ikonra klikkel, amik nem egyeznek meg,
+        /// addig elszámol háromnegyed másodpercig,
+        /// majd leáll és elrejti mindkét ikont
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Megállítja a stoppert
+            timer1.Stop();
+
+            // Elrejti mindkét ikont
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            // Visszaállítja a firstClicked és secondClicked 
+            // értékét, így legközelebb ha címkére kattint,
+            // akkor tudja majd a program, hogy ez lesz az első kattintás
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+
+
+
+
+
+
 
     }
 }
