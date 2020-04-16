f15b707c9163882f69e1b9e31abe05f38ab98888
fulopd <fulopdaniel@gmail.com>
2020-04-16 20:39:21 +0200

    Ikonok rendelése címkékhez

--- Memoriajatek/Form1.cs
+++ Memoriajatek/Form1.cs
@@ -25,9 +25,52 @@ namespace Memoriajatek
             "-", "-", "#", "#", "$", "$", "b", "b"
         };
 
+        
+       
+       
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+
         }
+
+
+
+
+        /// <summary>
+        /// Minden ikont hozzárendel egy véletlenszerű négyzethez
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            // A TableLayoutPanel-nek 16 címkéje van,
+            // és a listának 16 ikonja,
+            // tehát véletlenszerűen mindegyik ikont hozzá kell adni
+            // mindegyik címkéhez a nézetben
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
